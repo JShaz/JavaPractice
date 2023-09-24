8e9f4da96177fb4c7d14cd684e8dceef9cdc8250
JShaz <jamaalshazier92@gmail.com>
2023-09-24 10:45:31 -0400

    update assignments

--- FirstClass/src/ForLoop.java
+++ FirstClass/src/ForLoop.java
@@ -9,60 +9,80 @@ public class ForLoop {
         int l;
         int m;
         int n;
-        int o;
-//Loop 1
-        for(i = 0; i <= 99; ++i){
-            System.out.println(i);
-        }
-//Loop 2
-       for(j = 0; j <= 99; ++j){
-           if(j % 2 != 0){
-               System.out.println(j);
-            }
-          }
-//Loop 3
-          for(k = 0; k <= 99; ++k){
-              if(k % 2 == 0){
-                  System.out.println(k);
-                }
+
+
+////Loop 1
+for(i = 0; i <= 99; ++i){
+    System.out.println(i);
+}
+
+////Loop 2
+for(j = 0; j <= 99; ++j){
+    if(j % 2 != 0){
+       System.out.println(j);
+    }
+}
+
+
+////Loop 3
+for(k = 0; k <= 99; ++k){
+  if(k % 2 == 0){
+      System.out.println(k);
+    }
+}
+
 //Loop 4
-               }
-            for(l = 0; l <= 99; ++l){
-                if(l % 2 != 1){
-                    System.out.println(l);
-                }
-             }
-//Loop 5
-            int oddSum = 0;
-            for(m = 0; m <= 99; m++){
-                if(m % 2 != 0){
-                   oddSum = oddSum + m;
-               }
-             }
-              System.out.println(oddSum);
+for(l = 0; l <= 99; ++l){
+    if(l % 2 != 1){
+    System.out.println(l);
+    }
+}
 
-//Loop 6
-       int evenSum = 0;
-        for(n = 0; n <= 99; n++){
-            if(n % 2 != 1){
-                evenSum = evenSum + n;
-            }
-          }
-        System.out.println(evenSum);
-//Loop 7
+////Loop 5
+int oddSum = 0;
+for(m = 0; m <= 99; m++){
+    if(m % 2 != 0){
+       oddSum = oddSum + m;
+    }
+}
+System.out.println(oddSum);
 
-        int y;
-        int z;
 
-        int sum;
-for(o = 0; 0 <= 99; ++o){
+////Loop 6
+int evenSum = 0;
+for(n = 0; n <= 99; n++){
+    if(n % 2 != 1){
+        evenSum = evenSum + n;
+    }
+  }
+System.out.println(evenSum);
 
-    z = scnr.nextInt();
-    y = scnr.nextInt();
-     sum = y + z;
-    System.out.println(o);
-     System.out.println(sum);
-        }
+
+////Loop 7
+System.out.print("Enter the first number: ");
+int numOne = scnr.nextInt();
+
+System.out.print("Enter the second number: ");
+int numTwo = scnr.nextInt();
+
+
+if (numOne > numTwo) {
+    System.out.println("Please enter the numbers in ascending order.");
+    return;
+}
+
+
+int totalSum = 0;
+
+
+for (int a = numOne; a <=numTwo; a++) {
+    totalSum += a;
+}
+System.out.print(totalSum);
+
+
+
+
 
 
     }
--- FirstClass/src/Invoice.java
+++ FirstClass/src/Invoice.java
@@ -4,26 +4,44 @@ public class Invoice {
 
         public static void main(String[]args){
             Scanner input = new Scanner(System.in);
-            int ticketPrice;
+            double ticketPrice;
+            int age;
             int attending;
-            String groupName;
-            int totalPrice;
+            String name;
+            double totalPrice;
+            double discount;
+            double taxes;
 
 
-            ticketPrice = 400;
+            ticketPrice = 400.00;
 
-            System.out.println("Enter number of guest: ");
-
+            System.out.println("Enter number of tickets: ");
             attending = input.nextInt();
-            System.out.println("Enter Group Name: ");
+
+            System.out.println( "Enter your age");
+            age = input.nextInt();
+
+            System.out.println("Enter Your Name: ");
+            name = input.next();
+
+            discount = 50.00;
+            taxes = .6;
+
+            totalPrice = (ticketPrice * attending) ;
+
+            if( age > 55){
+                System.out.print("You qualify for a discount");
+                totalPrice = totalPrice - discount;
+
+            }
+            else{
+
+            }
 
 
-            groupName = input.next();
-
-            totalPrice = ticketPrice * attending;
 
             System.out.println("Invoice");
-            System.out.println("Group: " + groupName);
+            System.out.println("Group: " + name);
             System.out.println("Number of guest: " + attending );
             System.out.println("Price per ticket: " + ticketPrice);
             System.out.println("Your total is: " + totalPrice);
